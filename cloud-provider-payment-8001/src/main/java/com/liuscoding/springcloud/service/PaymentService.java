08ef2d459005bcf5858db67f223d2ac0f3c55165
jiezhenhua05 <379134775@qq.com>
2022-02-10 15:24:01 +0800

    dev

--- cloud-provider-payment-8001/src/main/java/com/liuscoding/springcloud/service/PaymentService.java
+++
@@ -1,9 +0,0 @@
-package com.liuscoding.springcloud.service;
-
-import com.liuscoding.springcloud.entity.Payment;
-
-public interface PaymentService {
-    int create(Payment payment);
-
-    Payment getPaymentById( Long id);
-}
